7889991aa9d2ff940ab1596ba995e9b7dab6b83c
carloskauan <carloskauan190@gmail.com>
2021-10-07 13:07:12 -0300

    Adicionando a lógica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,17 @@ export class AppComponent {
   codigoRMA:number = 12345;
   nome:string = "Carlos";
   //Notas
-  nota1:number = 8.5;
+  nota1:number = 10;
   nota2:number = 10;
-  nota3:number = 7;
-  nota4:number = 9;
+  nota3:number = 8;
+  nota4:number = 10;
   //Cálculo da Média
   media:number = ( this.nota1 + this.nota2 + this.nota3 + this.nota4 ) / 4;
   //Data e Imagem
   data:string = '2021/10/06';
   imagem:string = 'assets/img/MinhaFoto.jpg';
+  //Status
+  aprovado:boolean = true;
 
   //Template String
   primeiraNota: string = `Primeira Nota ${this.nota1}`;
