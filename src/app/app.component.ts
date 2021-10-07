c5d6c7493daf389fecfe0e57850c2eb5ba97c724
carloskauan <carloskauan190@gmail.com>
2021-10-06 22:09:59 -0300

    Montando estruturas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculoMedia';
+
+  title = 'Cálculo de Média';
+  codigoRMA:number = 12345;
+  nome:string = "Carlos";
+  //Notas
+  nota1:number = 8.5;
+  nota2:number = 10;
+  nota3:number = 7;
+  nota4:number = 9;
+  //Cálculo da Média
+  media:number = ( this.nota1 + this.nota2 + this.nota3 + this.nota4 ) / 4;
+  //Data e Imagem
+  data:string = '2021/10/06';
+  imagem:string = 'assets/img/MinhaFoto.jpg';
+
+  //Template String
+  primeiraNota: string = `Primeira Nota ${this.nota1}`;
+  segundaNota: string = `Segunda Nota ${this.nota2}`;
+  terceiraNota: string = `Terceira Nota ${this.nota3}`;
+  quartaNota: string = `Quarta Nota ${this.nota4}`;
+  resultado: string = `Sua média final é : `;
+
+
 }
